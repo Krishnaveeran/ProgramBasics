90c237d0197c1a123228edfc438fa0e1599801ed
Krishnaveeran <dhlipkrishna@gmail.com>
2022-10-07 19:59:44 +0530

    Arithmetic Operator

---
+++ RelOperator.java
@@ -0,0 +1,28 @@
+package programmBasics;
+
+public class RelOperator {
+public static void main(String[] args) {
+	int mark1=1;
+	int mark2=2;
+	
+	if (mark1>mark2)
+		System.out.println("1>2");
+	if (mark1>=mark2)
+		System.out.println("1>2");
+	if (mark1<mark2)
+		System.out.println("1>2 <");
+	if (mark1<=mark2)
+		System.out.println("1>2 <=");
+	if (mark1!=mark2)
+		System.out.println("1 not equal 2");
+	int value1 = 1;
+    int value2 = 2;
+    if((value1 == 1) && (value2 == 2))
+        System.out.println("value1 is 1 AND value2 is 2");
+    if((value1 == 1) && (value2 == 1))
+        System.out.println("value1 is 1 OR value2 is 1");
+	
+	
+	
+}
+}
---
+++ TenaliBeats.java
@@ -0,0 +1,12 @@
+package programmBasics;
+
+public class TenaliBeats {
+public static void main(String[] args) {
+	int beats=512;
+	while(beats>1) 
+	{
+		beats=(beats/2);
+	System.out.println(beats);
+}
+}
+}
