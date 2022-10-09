eb9dce0e8abad3075e36cb19628fce07980ca10e
Krishnaveeran <dhlipkrishna@gmail.com>
2022-10-09 13:19:04 +0530

    If else program

---
+++ IfElse5.java
@@ -0,0 +1,11 @@
+package programmBasics;
+
+public class IfElse5 {
+	public static void main(String[] args) {
+		int x=1;
+		if(1)
+		System.out.println("True");
+		else
+		System.out.println("False");
+	}
+}
