ecd769bf5b04012a4de3009abb052265986e023c
Krishnaveeran <dhlipkrishna@gmail.com>
2022-10-12 19:37:04 +0530

    Pious man program

---
+++ PiousMan.java
@@ -0,0 +1,15 @@
+package programmBasics;
+
+public class PiousMan {
+public static void main(String[] args) {
+	int count=0;
+	int sum=1;
+	while(count<=7) 
+	{
+		sum=sum*2;
+		count++;
+		System.out.println(sum);
+	
+}
+}
+}
---
+++ PiousMan2.java
@@ -0,0 +1,16 @@
+package programmBasics;
+
+public class PiousMan2 {
+	public static void main(String[] args) {
+
+	int count=1;
+	int sum=1;
+	
+	while(count<=7)
+	{
+		sum=sum*2;
+		count++;
+	}
+	System.out.println(sum);
+}
+}
