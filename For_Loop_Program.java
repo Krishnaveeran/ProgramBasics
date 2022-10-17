ca8ab939b4e6bdd6beb3900dfbbbc95562700ff0
Krishnaveeran <dhlipkrishna@gmail.com>
2022-10-17 16:12:11 +0530

    new message

---
+++ For_Loop_Program.java
@@ -0,0 +1,276 @@
+package programmBasics;
+
+public class For_Loop_Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		For_Loop_Program flp=new For_Loop_Program();
+//		flp.for_loop_program1();	
+//		flp.for_loop_program2();	
+//		flp.for_loop_program3();
+//		flp.for_loop_program4();
+//		flp.for_loop_program5();
+//		flp.for_loop_program6();
+//		flp.for_loop_program7();
+//		flp.for_loop_program8();
+//		flp.for_loop_program9();
+//		flp.for_loop_program10();
+//		flp.for_loop_program11();
+//		flp.for_loop_program12();
+//		flp.for_loop_program13();
+	
+		
+	}
+
+//	private void for_loop_program13() {
+//		// TODO Auto-generated method stub
+//		for(int row=5; row>=1; row-- )
+//		{
+//		for(int col=1; col<=row; col++)
+//		{
+//			System.out.print("* ");
+//		}
+//		for(int col=5; col>=row+1; col--)
+//		{
+//			System.out.print("    ");
+//		}
+//		for(int num=1; num<=row; num++)
+//		{
+//			System.out.print("* ");
+//		}
+//		System.out.println();
+//		}
+//}
+//}
+//	private void for_loop_program12() {
+//		// TODO Auto-generated method stub
+//		int no1=1;
+//		for(int row=1; row<=5; row++)
+//		{
+//		for(int col=1; col<=no1; col++)
+//		{
+//			System.out.print("*");
+//		}
+//		System.out.println(); no1+=2;
+//		}
+//	}
+//}
+
+//	private void for_loop_program11() {
+		// TODO Auto-generated method stub
+//		for(int row = 1; row<=5; row++)
+//		{
+//		for(int star=1; star<=5-row; star++)
+//		    {   System.out.print("* ");     }
+//		    for(int col=1; col<=row; col++)
+//		      {  System.out.print(col+" ");   }
+//		System.out.println();
+//		}}}
+//		for(int row=1; row<=5; row++)
+//		{
+//			for(int star=5-row; star>=0; star--)
+//			{
+//				System.out.print("* ");
+//			}
+//			for(int col=1; col<=row; col++)
+//			{
+//				System.out.print(col+" ");
+//			}
+//			System.out.println();
+//		}
+//	}	
+//}
+//		for(int row=4; row>=0; row--)
+//		{
+//		for(int star=1; star<=row; star++)
+//		{
+//			System.out.print(" "+ " ");	
+//		}
+//		for(int col=1; col<=5-row; col++)
+//		{
+//			System.out.print(col+" ");
+//		}
+//		System.out.println();
+//		}
+//		
+//	}}
+//	private void for_loop_program10() {
+//		// TODO Auto-generated method stub
+//		for(int row=1; row<=5; row++)
+//		{
+//		for(int col=1; col<row; col++)
+//		{
+//			System.out.print(" " +" ");
+//		}
+//		for(int star=1; star<=6-row; star++)
+//		{
+//			System.out.print((star*row)+ " ");
+//		}
+//		System.out.println();
+//		}
+//	}
+//}
+//	private void for_loop_program9() {
+		// TODO Auto-generated method stub
+//		int var=15;
+//		for(int row=5; row>=1; row--)
+//		{
+//		for(int col=5; col>=row; col--)
+//		{
+//			System.out.print(var--+ " ");
+//		}
+//		System.out.println();
+//		}
+//		for(int row=1; row<=5; row++)
+//		{
+//		for (int col=1; col<=row; col++)
+//		{
+//			System.out.print((col*row)+ " ");
+//		}
+//		System.out.println();
+//		}
+//		int var=1;
+//		for(int row=1; row<=5; row++)
+//		{
+//		for(int col=1; col<=row; col++)
+//		{
+//			System.out.print(var++ + " ");
+//		}
+//		System.out.println();
+//		}
+//		for(int row=5; row>=1; row--)
+//		{
+//		for (int col=5; col>=row; col--)
+//		{
+//			System.out.print(row+ " ");
+//		}
+//		System.out.println();
+//		}
+	
+//		for(int row=5; row>=1; row--)
+//		{
+//		for (int col=5; col>=row; col--)
+//		{
+//			System.out.print(col+" ");
+//		}
+//		System.out.println();
+//		}
+		
+//		for(int row=1; row<=5; row++)
+//		{
+//		for (int col=1; col<=row; col++)
+//		{
+//			System.out.print(col+" ");
+//		}
+//		System.out.println();
+//		
+//	}
+//}
+//}
+
+//private void for_loop_program8() {
+//		// TODO Auto-generated method stub
+//	int num=1;
+//	for(int row=1; row<=5; row++)
+//	{
+//	for (int col=1; col<=6-row; col++)
+//	{
+//		System.out.print(num++ + " ");
+//	}
+//	System.out.println();
+//	}	
+//}
+//}
+
+
+
+//	private void for_loop_program7() {
+//		// TODO Auto-generated method stub
+//		for(int row = 1; row<=5; row++)
+//		  { 
+//		  for(int col = 1; col<row; col++)
+//		   { System.out.print(" "+" "); }
+//		  
+//		  for(int col=1; col<=6-row; col++)
+//		   {System.out.print(col + " "); }
+//		  
+//		   System.out.println(); 
+//		  }
+//
+//	}
+//}
+//	private void for_loop_program6() {
+//		// TODO Auto-generated method stub
+//		for(int row=5; row>=1; row--)
+//			{
+//			for(int col=1; col<=row; col++)
+//			{
+//				System.out.print("*"+" ");
+//			}
+//		System.out.println();
+//	}
+//}
+//}
+//	private void for_loop_program5() {
+//		// TODO Auto-generated method stub
+//		for(char row='E'; row>='A'; row--)
+//		{
+//		for(char col='A'; col<=row; col++)
+//		{
+//			System.out.print(col+" ");
+//		}
+//	System.out.println();			
+//	}
+//}
+//}
+//	private void for_loop_program4() {
+//		// TODO Auto-generated method stub
+//		for(int row=5; row>=1; row--)
+//		{
+//		for(int col=5; col>=row; col--)
+//		{
+//			System.out.print(col+" ");
+//		}
+//		System.out.println();
+//		}
+//}
+//}
+
+//	private void for_loop_program3() {
+//		// TODO Auto-generated method stub
+//		for(int row=1; row<=5; row++)
+//		{
+//		for(int col=5; col>=row; col--)
+//		{
+//			System.out.print(col+ " ");
+//		}
+//		System.out.println();
+//	}
+//}
+//}
+
+//	private void for_loop_program2() {
+//		// TODO Auto-generated method stub
+//		for(int row=1; row<=5; row++)
+//		{
+//			for(int col=1; col<=row ; col++)
+//		{
+//			System.out.print(col+ " ");
+//		}
+//			System.out.println();
+//	}
+//}
+//}
+
+//	private void for_loop_program1() {
+//		// TODO Auto-generated method stub
+//	for(int row=5; row>=1; row--)	
+//	{
+//		for(int col=1; col<=row; col++)
+//		{
+//			System.out.print(col+" ");
+//		}
+//		System.out.println();
+//	}
+//}
+//}
