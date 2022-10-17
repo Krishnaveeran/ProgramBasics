4a2baf9e23f0ed054670ee650e41bd5266918688
Krishnaveeran <dhlipkrishna@gmail.com>
2022-10-17 22:42:48 +0530

    Number series Program

---
+++ NumberSeries.java
@@ -0,0 +1,16 @@
+package programmBasics;
+
+public class NumberSeries {
+	public static void main(String[] args) {
+		int count=7;
+		int flower=1;
+	while(count>=1)
+	{
+		flower=flower*2;
+		count=count-1;
+	}
+	System.out.println(flower);
+	
+}
+}
+
