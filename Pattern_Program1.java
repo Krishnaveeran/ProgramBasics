4c36d7a4c72d11571c82b3bc5abf4bcd4e1c1ef0
Krishnaveeran <dhlipkrishna@gmail.com>
2022-10-18 11:37:12 +0530

    32 Patter program in java

---
+++ Pattern_Program1.java
@@ -0,0 +1,945 @@
+package programmBasics;
+
+public class Pattern_Program1 
+{
+	public static void main(String[] args) {
+		Pattern_Program1 pp=new Pattern_Program1();
+//		pp.pattern_program1();
+//		pp.pattern_program2();
+//		pp.pattern_program3();
+//		pp.pattern_program4();
+//		pp.pattern_program5();
+//		pp.pattern_program6();
+//		pp.pattern_program7();
+//		pp.pattern_program8();
+//		pp.pattern_program9();
+//		pp.pattern_program10();
+//		pp.pattern_program12();
+//		pp.pattern_program13();
+//		pp.pattern_program14();
+//		pp.pattern_program15();
+//		pp.pattern_program16();
+//		pp.pattern_program17();
+//		pp.pattern_program18();
+//		pp.pattern_program19();
+//		pp.pattern_program20();
+//		pp.pattern_program21();
+//		pp.pattern_program22();
+//		pp.pattern_program23();
+//		pp.pattern_program24();
+//		pp.pattern_program25();
+//		pp.pattern_program26();
+//		pp.pattern_program27();
+//		pp.pattern_program28();
+//		pp.pattern_program29();
+//		pp.pattern_program30();
+//		pp.pattern_program31();
+		pp.pattern_program32();
+	}
+
+	private void pattern_program32() {
+		// TODO Auto-generated method stub
+        int n= 6;
+
+for(int row=1;row<=n*2-1;row++)
+{
+
+for(int col=1;col<=n*2-1;col++)
+
+{      if(col==row || col==(n*2)-row){
+System.out.print("*");
+
+}
+
+System.out.print(" ");
+
+}
+
+System.out.println();
+		
+	}
+	}
+}
+
+//	private void pattern_program31() {
+//		// TODO Auto-generated method stub
+//		
+//		int num=1;
+//		for(int row=1; row<=4; row++)
+//		{
+//		for(int col=1; col<row; col++)
+//		{
+//		System.out.print(" "+" ");
+//		}
+//		for(int col=1; col<=10-num; col++)
+//		{
+//			if(col==1 || col== 10-num )
+//			System.out.print("* ");
+//			else
+//			System.out.print("  ");
+//		}num+=2;
+//		System.out.println();
+//		}
+//
+//		int num1=1;
+//		for(int row=5; row>=1; row--)
+//		{
+//		for(int col=1; col<row; col++)
+//		{
+//			System.out.print(" "+" ");
+//		}
+//		for(int col=1; col<=num1; col++)
+//		{
+//			if(col==1 || col==num1  )
+//			System.out.print("* ");
+//			else
+//				System.out.print("  ");
+//		}num1+=2;
+//		System.out.println();
+//	}
+//		
+//	}
+//
+//	}
+
+	
+	
+//	private void pattern_program30() {
+//		// TODO Auto-generated method stub
+//				
+//		for(int row=1; row<=5; row++)
+//		{
+//		for(int col=1; col<=5; col++)
+//		{
+//			if(col==1 || col==5 || row==1 || row==5)
+//			System.out.print("* ");
+//			else
+//			System.out.print("  ");
+//		}
+//		System.out.println();
+//	}
+//		
+//	}
+//}
+//	private void pattern_program29() {
+//		// TODO Auto-generated method stub
+//		for(int row=1; row<=5; row++)
+//		{
+//		for(int col=1; col<=5; col++)
+//		{
+//			System.out.print("* ");
+//		}
+//		System.out.println();
+//	}
+//}
+//}	
+	
+	
+//	private void pattern_program28() {
+		// TODO Auto-generated method stub
+		
+//		int num=1;
+//		for(int row=1; row<=4; row++)
+//		{
+//		for(int col=1; col<row; col++)
+//		{
+//		System.out.print(" "+" ");
+//		}
+//		for(int col=1; col<=10-num; col++)
+//		{
+//			if(col==1 || col== 10-num || row==1)
+//			System.out.print("* ");
+//			else
+//			System.out.print("  ");
+//		}num+=2;
+//		System.out.println();
+//		}
+//
+//		int num1=1;
+//		for(int row=5; row>=1; row--)
+//		{
+//		for(int col=1; col<row; col++)
+//		{
+//			System.out.print(" "+" ");
+//		}
+//		for(int col=1; col<=num1; col++)
+//		{
+//			if(col==1 || col==num1 || row==1 )
+//			System.out.print("* ");
+//			else
+//				System.out.print("  ");
+//		}num1+=2;
+//		System.out.println();
+//	}
+//		
+//	}
+//}
+	
+	
+//	private void pattern_program27() {
+//		// TODO Auto-generated method stub
+//		
+//		int num1=1;
+//		for(int row=5; row>=2; row--)
+//		{
+//		for(int col=1; col<row; col++)
+//		{
+//			System.out.print(" "+" ");
+//		}
+//		for(int col=1; col<=num1; col++)
+//		{
+//			if(col==1 || col==num1 || row==1 )
+//			System.out.print("* ");
+//			else
+//				System.out.print("  ");
+//		}num1+=2;
+//		System.out.println();
+//	}
+//		
+//		int num2=1;
+//		for(int row=1; row<=5; row++)
+//		{
+//		for(int col=1; col<row; col++)
+//		{
+//		System.out.print(" "+" ");
+//		}
+//		for(int col=1; col<=10-num2; col++)
+//		{
+//			if(col==1 || col== 10-num2)
+//			System.out.print("* ");
+//			else
+//			System.out.print("  ");
+//		}num2+=2;
+//		System.out.println();
+//		}
+//		
+//		
+//	}
+//}
+
+
+
+//	private void pattern_program26() {
+//		// TODO Auto-generated method stub
+//		int num=1;
+//		for(int row=1; row<=5; row++)
+//		{
+//		for(int col=1; col<row; col++)
+//		{
+//		System.out.print(" "+" ");
+//		}
+//		for(int col=1; col<=10-num; col++)
+//		{
+//			if(col==1 || col== 10-num || row==1)
+//			System.out.print("* ");
+//			else
+//			System.out.print("  ");
+//		}num+=2;
+//		System.out.println();
+//		}
+//	}
+//}
+
+
+//	private void pattern_program25() {
+//		// TODO Auto-generated method stub
+//		int num=1;
+//		for(int row=5; row>=1; row--)
+//		{
+//		for(int col=1; col<row; col++)
+//		{
+//			System.out.print(" "+" ");
+//		}
+//		for(int col=1; col<=num; col++)
+//		{
+//			if(col==1 || col==num || row==1 )
+//			System.out.print("* ");
+//			else
+//				System.out.print("  ");
+//		}num+=2;
+//		System.out.println();
+//	}
+//	}
+//}
+
+
+//	private void pattern_program24() {
+//		// TODO Auto-generated method stub
+//		
+//		for(int row=1; row<=4; row++)  //changed row as 4
+//		{
+//		for(int col=1; col<row; col++)
+//		{
+//			System.out.print(" "+" ");
+//		}
+//		for(int col=1; col<=6-row; col++ )
+//		{
+//			if(col==1 || col==6-row || row==1)
+//			System.out.print("* ");
+//			else
+//				System.out.print("  ");
+//		}
+//		System.out.println();
+//	}
+//		
+//		
+//		for(int row=5; row>=1; row--)
+//		{
+//		for(int col=1; col<row; col++)
+//		{
+//			System.out.print(" "+" ");
+//		}
+//		for(int col=1; col<=6-row; col++)
+//		{
+//			if(col==1 || col==6-row || row==1 )
+//			System.out.print("* ");
+//			else
+//				System.out.print("  ");
+//		}
+//		System.out.println();
+//	}
+//		
+//	}
+//}	
+	
+
+//	private void pattern_program23() {
+//		// TODO Auto-generated method stub
+//		
+//		for(int row=5; row>=2; row--) //changed row as 2
+//		{
+//		for(int col=1; col<row; col++)
+//		{
+//			System.out.print(" "+" ");
+//		}
+//		for(int col=1; col<=6-row; col++)
+//		{
+//			if(col==1 || col==6-row || row==1 ) 
+//			System.out.print("* ");
+//			else
+//				System.out.print("  ");
+//		}
+//		System.out.println();
+//	}
+//
+//		for(int row=1; row<=5; row++)
+//		{
+//		for(int col=1; col<row; col++)
+//		{
+//			System.out.print(" "+" ");
+//		}
+//		for(int col=1; col<=6-row; col++ )
+//		{
+//			if(col==1 || col==6-row ) //removed row==1 condition from if
+//			System.out.print("* ");
+//			else
+//				System.out.print("  ");
+//		}
+//		System.out.println();
+//	}
+//}		
+//			
+//}
+
+//	private void pattern_program22() {
+//		// TODO Auto-generated method stub
+//		
+//		for(int row=5; row>=2; row--) // changed row >=2 from >=1
+//		{
+//		for(int col=1; col<=row; col++)
+//		{
+//			if(col==1 || row==col || 5==row)
+//			{
+//			System.out.print("* ");
+//			}
+//			else
+//			{
+//				System.out.print("  ");	
+//			}
+//		}
+//		System.out.println();
+//		}
+//		
+//		
+//		for(int row=1; row<=5; row++)
+//		{
+//		for(int col=1; col<=row; col++)	
+//		{
+//			if(col==1 || col==row || row==5) 
+//			{System.out.print("* ");}
+//			else
+//			{
+//				System.out.print("  ");
+//			}
+//		}
+//		System.out.println();
+//		}
+//		}
+//		
+//	}
+	
+	
+
+//	private void pattern_program21() {
+//		// TODO Auto-generated method stub
+//		
+//		
+//		for(int row=1; row<=4; row++)  // Changed row as 4
+//		{
+//		for(int col=1; col<=row; col++)	
+//		{//		for(int row=1; row<=5; row++)
+//{
+//for(int col=1; col<row; col++)
+//{
+//	System.out.print(" "+" ");
+//}
+//for(int col=1; col<=6-row; col++ )
+//{
+//	if(col==1 || col==6-row || row==1)
+//	System.out.print("* ");
+//	else
+//		System.out.print("  ");
+//}
+//System.out.println();
+//}
+//}		
+//}	
+//			if(col==1 || col==row || row==5) 
+//			{System.out.print("* ");}
+//			else
+//			{
+//				System.out.print("  ");
+//			}//		for(int row=1; row<=5; row++)
+//{
+//for(int col=1; col<row; col++)
+//{
+//	System.out.print(" "+" ");
+//}
+//for(int col=1; col<=6-row; col++ )
+//{
+//	if(col==1 || col==6-row || row==1)
+//	System.out.print("* ");
+//	else
+//		System.out.print("  ");
+//}
+//System.out.println();
+//}
+//}		
+//}	
+//		}
+//		System.out.println();
+//		}
+//		
+//		
+//		for(int row=5; row>=1; row--)
+//		{
+//		for(int col=1; col<=row; col++)
+//		{
+//			if(col==1 || row==col ) // removed one condition 5==row
+//			{
+//			System.out.print("* ");
+//			}//		for(int row=1; row<=5; row++)
+//{
+//for(int col=1; col<row; col++)
+//{
+//	System.out.print(" "+" ");
+//}
+//for(int col=1; col<=6-row; col++ )
+//{
+//	if(col==1 || col==6-row || row==1)
+//	System.out.print("* ");
+//	else
+//		System.out.print("  ");
+//}
+//System.out.println();
+//}
+//			else
+//			{
+//				System.out.print("  ");	
+//			}
+//		}
+//		System.out.println();
+//		}
+//		
+//	}
+//}
+
+
+
+//	private void pattern_program20() {
+//		// TODO Auto-generated method stub
+//		
+//		for(int row=1; row<=5; row++)
+//		{
+//		for(int col=1; col<row; col++)
+//		{
+//			System.out.print(" "+" ");
+//		}
+//		for(int col=1; col<=6-row; col++ )
+//		{
+//			if(col==1 || col==6-row || row==1)
+//			System.out.print("* ");
+//			else
+//				System.out.print("  ");
+//		}
+//		System.out.println();
+//	}
+//}		
+//}	
+		
+	
+
+//	private void pattern_program19() {
+		// TODO Auto-generated method stub
+		
+//		for(int row=5; row>=1; row--)
+//		{
+//		for(int col=1; col<row; col++)
+//		{
+//			System.out.print(" "+" ");
+//		}
+//		for(int col=1; col<=6-row; col++)
+//		{
+//			if(col==1 || col==6-row || row==1 )
+//			System.out.print("* ");
+//			else
+//				System.out.print("  ");
+//		}
+//		System.out.println();
+//	}
+//		
+//	}
+//}
+
+
+//	private void pattern_program18() {
+//		// TODO Auto-generated method stub
+//	
+//		for(int row=5; row>=1; row--)
+//		{
+//		for(int col=1; col<=row; col++)
+//		{
+//			if(col==1 || row==col || 5==row)
+//			{
+//			System.out.print("* ");
+//			}
+//			else
+//			{
+//				System.out.print("  ");	
+//			}
+//		}
+//		System.out.println();
+//		}
+//	}
+//}
+//	
+
+	
+	
+//	private void pattern_program17() {
+//		// TODO Auto-generated method stub 
+//	
+//	for(int row=1; row<=5; row++)
+//	{
+//	for(int col=1; col<=row; col++)	
+//	{
+//		if(col==1 || col==row || row==5) 
+//		{System.out.print("* ");}
+//		else
+//		{
+//			System.out.print("  ");
+//		}
+//	}
+//	System.out.println();
+//	}
+//	}
+//}
+	
+
+//	private void pattern_program16() {
+//		// TODO Auto-generated method stub
+//		int no=0;
+//		for(int row=5; row>=2; row--)
+//		{
+//		for(int col=1; col<=row; col++)
+//		{
+//				System.out.print("* ");
+//		}
+//		for(int col=1; col<=(5-row)+no; col++)
+//		{
+//			System.out.print("  ");
+//		}no+=1;
+//		for(int col=1; col<=row; col++)
+//		{
+//			System.out.print("* ");
+//		}
+//		System.out.println();
+//		}
+//		
+//		
+//		int no1=1;
+//		for(int row=1; row<=5; row++)  // if we change row as 4 we remove extra line
+//		{
+//		for(int col=1; col<=row; col++)
+//		{
+//			System.out.print("* ");
+//		}
+//		for(int col=1; col<=9-no1; col++)
+//		{
+//		System.out.print("  ");	
+//		}	no1+=2;
+//		for(int col=1; col<=row; col++)
+//		{
+//			System.out.print("* ");
+//		}
+//		System.out.println();
+//		}	
+//			
+//	}
+//}	
+
+
+
+//	private void pattern_program15() {
+//	
+//	int no1=1;
+//	for(int row=1; row<=4; row++)
+//	{
+//	for(int col=1; col<=row; col++)
+//	{
+//		System.out.print("* ");
+//	}
+//	for(int col=1; col<=9-no1; col++)
+//	{
+//	System.out.print("  ");	
+//	}	no1+=2;
+//	for(int col=1; col<=row; col++)
+//	{
+//		System.out.print("* ");
+//	}
+//	System.out.println();
+//	}
+//	
+//	
+//	int no=0;
+//	for(int row=5; row>=1; row--)
+//	{
+//	for(int col=1; col<=row; col++)
+//	{
+//			System.out.print("* ");
+//	}
+//	for(int col=1; col<=(5-row)+no; col++)
+//	{
+//		System.out.print("  ");
+//	}no+=1;
+//	for(int col=1; col<=row; col++)
+//	{
+//		System.out.print("* ");
+//	}
+//	System.out.println();
+//	}
+//	
+//}
+//}
+
+		
+//	private void pattern_program14() {
+//		// TODO Auto-generated method stub
+//		int no=0;
+//		for(int row=5; row>=1; row--)
+//		{
+//		for(int col=1; col<=row; col++)
+//		{
+//				System.out.print("* ");
+//		}
+//		for(int col=1; col<=(5-row)+no; col++)
+//		{
+//			System.out.print("  ");
+//		}no+=1;
+//		for(int col=1; col<=row; col++)
+//		{
+//			System.out.print("* ");
+//		}
+//		System.out.println();
+//		}
+//	}
+//}
+	
+	
+//	private void pattern_program13() {
+//		// TODO Auto-generated method stub
+//		int no1=1;
+//		for(int row=1; row<=5; row++)
+//		{
+//		for(int col=1; col<=row; col++)
+//		{
+//			System.out.print("* ");
+//		}
+//		for(int col=1; col<=9-no1; col++)
+//		{
+//		System.out.print("  ");	
+//		}	no1+=2;
+//		for(int col=1; col<=row; col++)
+//		{
+//			System.out.print("* ");
+//		}
+//		System.out.println();
+//		}
+//	}
+//}
+	
+	
+	
+//	private void pattern_program12() {
+//		// TODO Auto-generated method stub
+//		int num=1;
+//		for(int row=1; row<=4; row++)  // change row from 5 to 4 to remove extra star present in line number 5 while merging
+//		{
+//		for(int col=1; col<row; col++)
+//		{
+//			System.out.print(" "+" ");
+//		}
+//		for(int col=1; col<=10-num; col++)
+//		{
+//			System.out.print("* ");
+//		} num+=2;
+//		System.out.println();
+//		}
+//		
+//		int no=0;
+//		for(int row=4; row>=0; row--)
+//		{
+//		for(int col=1; col<=row; col++)
+//		{
+//			System.out.print(" "+" ");
+//		}
+//		for(int col=1; col<=(5-row)+no; col++)
+//		{
+//			System.out.print("* ");
+//		} no+=1;
+//		System.out.println();
+//		}
+//	}
+//}
+	
+	
+//	private void pattern_program10() {
+//		// TODO Auto-generated method stub
+//		int num=1;
+//		for(int row=1; row<=5; row++)
+//		{
+//		for(int col=1; col<row; col++)
+//		{
+//			System.out.print(" "+" ");
+//		}
+//		for(int col=1; col<=10-num; col++)
+//		{
+//			System.out.print("* ");
+//		} num+=2;
+//		System.out.println();
+//		}
+//	}
+//}
+
+	
+	
+//	private void pattern_program9() {
+//		// TODO Auto-generated method stub
+//		int no=0;
+//		for(int row=4; row>=0; row--)
+//		{
+//		for(int col=1; col<=row; col++)
+//		{
+//			System.out.print(" "+" ");
+//		}
+//		for(int col=1; col<=(5-row)+no; col++)
+//		{
+//			System.out.print("* ");
+//		} no+=1;
+//		System.out.println();
+//		}
+//	}
+//}
+	
+	
+//	private void pattern_program8() {
+//		// TODO Auto-generated method stub
+//		
+//		
+//	for(int row=1; row<=4; row++)
+//	{
+//	for(int col=1; col<row; col++)
+//	{
+//		System.out.print(" "+" ");
+//	}
+//	for(int col=1; col<=6-row; col++ )
+//	{
+//		System.out.print("* ");
+//	}
+//	System.out.println();
+//	}
+//	
+//		
+//	for(int row=5; row>=1; row--)
+//	{
+//	for(int col=1; col<row; col++)
+//	{
+//		System.out.print(" "+" ");
+//	}
+//	for(int col=1; col<=6-row; col++)
+//	{
+//		System.out.print("* ");
+//	}
+//	System.out.println();
+//}
+//}
+//}
+
+
+	
+//	private void pattern_program7() {
+//		// TODO Auto-generated method stub
+//		for(int row=5; row>=1; row--)
+//		{
+//		for(int col=1; col<row; col++)
+//		{
+//			System.out.print(" "+" ");
+//		}
+//		for(int col=1; col<=6-row; col++)
+//		{
+//			System.out.print("* ");
+//		}
+//		System.out.println();
+//		}
+//		
+//		for(int row=1; row<=5; row++)
+//		{
+//		for(int col=1; col<row; col++)
+//		{
+//			System.out.print(" "+" ");
+//		}
+//		if(row!=1) {
+//		for(int col=1; col<=6-row; col++ )
+//		{
+//			System.out.print("* ");
+//		}
+//		System.out.println();}
+//	}
+//	}	
+
+
+
+//	private void pattern_program6() {
+//		// TODO Auto-generated method stub
+//		for(int row=5; row>=1; row--)
+//		{
+//		for (int col=1; col<=row; col++)
+//		{
+//			System.out.print("* ");
+//		}System.out.println();
+//		}
+//		
+//		for(int row=1; row<=5; row++)
+//		{
+//			if(row!=1)
+//			{
+//		for(int col=1; col<=row; col++)
+//		{
+//			System.out.print("* ");
+//		}
+//		System.out.println();
+//		}
+//}
+//}
+
+
+
+//	private void pattern_program5() {
+//		// TODO Auto-generated method stub	
+//		
+//		int n=5;
+//		for(int row=1; row<=5; row++)
+//		{
+//		for(int col=1; col<=row; col++)
+//		{
+//			System.out.print("* ");
+//		}System.out.println();
+//		}
+//		
+//		for(int row=1; row<=5; row++)
+//		{
+//		
+//		for(int col=n-1; col>=row; col--)  // We can give if condition if(!=1)
+//		{
+//			System.out.print("* ");
+//		}System.out.println();
+//		}
+//}
+//}
+
+
+
+//	private void pattern_program4() {
+//		// TODO Auto-generated method stub
+//		for(int row=1; row<=5; row++)
+//		{
+//		for(int col=1; col<row; col++)
+//		{
+//			System.out.print(" "+" ");
+//		}
+//		for(int col=1; col<=6-row; col++ )
+//		{
+//			System.out.print("* ");
+//		}
+//		System.out.println();
+//	}
+//}
+//}
+
+
+//	private void pattern_program3() {
+//		// TODO Auto-generated method stub
+//		for(int row=5; row>=1; row--)
+//		{
+//		for(int col=1; col<row; col++)
+//		{
+//			System.out.print(" "+" ");
+//		}
+//		for(int col=1; col<=6-row; col++)
+//		{
+//			System.out.print("* ");
+//		}
+//		System.out.println();
+//	}
+//}
+//}
+
+
+
+//	private void pattern_program2() {
+//		// TODO Auto-generated method stub
+//		for(int row=5; row>=1; row--)
+//		{
+//		for(int col=1; col<=row; col++)
+//		{
+//			System.out.print("* ");
+//		}
+//		System.out.println();
+//	}
+//	}		
+//}
+//	
+
+
+//	private void pattern_program1() {
+//		// TODO Auto-generated method stub
+//		for (int row=1; row<=5; row++)
+//		{
+//		for(int col=1; col<=row; col++)
+//		{
+//			System.out.print("* ");
+//		}
+//		System.out.println();
+//	}
+//}
+//}
