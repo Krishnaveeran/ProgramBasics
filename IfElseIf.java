072a8b8ae609bf0688f8e0e804cf88ef9a4ae47d
Krishnaveeran <dhlipkrishna@gmail.com>
2022-10-20 13:02:26 +0530

    If else if program

---
+++ IfElseIf.java
@@ -0,0 +1,20 @@
+package programmBasics;
+
+public class IfElseIf 
+{
+	public static void main(String[] args) {
+int age=20;
+System.out.println(50%100);
+if(age <=18 && age >=12){
+	System.out.println("age between 12 and 18 ");
+}
+else if(age >=22 && age <=25){
+	System.out.println("age between 22 and 25 ");
+}
+else if(age <=35 && age >=26){
+	System.out.println("age between 26 and 35 ");
+}
+else{
+	System.out.println("print this one");
+}}
+}
