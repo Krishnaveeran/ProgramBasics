730363624c90228676f4a8ad76e55e0c920e5815
Krishnaveeran <dhlipkrishna@gmail.com>
2022-11-16 10:20:57 +0530

    Stack overflow error

---
+++ StackOverFlowDemo.java
@@ -0,0 +1,26 @@
+package arrayafter;
+
+public class StackOverFlowDemo {
+
+	
+	 public static void main(String[] args) {
+		  // TODO Auto-generated method stub
+		  
+		 StackOverFlowDemo ed = new StackOverFlowDemo(); 
+		  
+		   ed.display();
+		   //throw: 
+		   // try   
+
+		 }
+
+		 private void display() {
+		  // TODO Auto-generated method stub
+		  System.out.println("Hi");
+		  
+		  display(); 
+		  
+		 }
+
+		}
+
